db044945d5cc5d75a4a7348eef229e4b690298fb
huy8895 <huy8895@gmail.com>
2025-05-13 14:57:20 +0700

    add card badges

--- trello_powerup/client.js
+++ trello_powerup/client.js
@@ -11,5 +11,34 @@ window.TrelloPowerUp.initialize({
         });
       }
     }];
-  }
+  },
+  'card-badges': function (t, options) {
+    return t.get('card', 'shared', 'flow')
+      .then(function (flow) {
+        return [{
+          text: flow,
+          color: 'blue', // hoặc 'green', 'red', 'purple', 'sky', 'orange'
+          icon: 'https://cdn-icons-png.flaticon.com/512/25/25231.png'
+        }];
+      });
+  },
+
+  'card-detail-badges': function (t, options) {
+    return t.get('card', 'shared')
+      .then(function (data) {
+        if (!data || !data.flow) return [];
+
+        return [{
+          title: 'Build Status',
+          text: data.flow,
+          callback: function (t) {
+            return t.popup({
+              title: 'Build Progress',
+              url: 'popup/popup.html', // hoặc popup khác nếu bạn có
+              height: 200
+            });
+          }
+        }];
+      });
+  },
 });

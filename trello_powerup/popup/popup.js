40518da3c29f5bf757c34fa1d26b06816f0d2f56
huy8895 <huy8895@gmail.com>
2025-05-13 12:02:12 +0700

    update popup.js

--- trello_powerup/popup/popup.js
+++ trello_powerup/popup/popup.js
@@ -1,27 +1,33 @@
-const t = TrelloPowerUp.iframe();
 
-document.getElementById('submit').addEventListener('click', async function() {
-  const driveLink = document.getElementById('driveLink').value;
-  const videoName = document.getElementById('videoName').value;
+
+window.onload = function () {
+  const iframe = TrelloPowerUp.iframe();
+
+  document.getElementById('submit').addEventListener('click', async function() {
+    const driveLink = document.getElementById('driveLink').value;
+    const videoName = document.getElementById('videoName').value;
     const flow = document.getElementById('flow').value;
 
-  const card = await t.card('name', 'id');
-  const board = await t.board('name');
+    const card = await iframe.card('name', 'id');
+    const board = await iframe.board('name');
 
-  await fetch("https://eo92jfgk4r4masz.m.pipedream.net", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify({
-      cardName: card.name,
-      cardId: card.id,
-      boardName: board.name,
-      driveLink,
-      videoName,
-      flow
-    })
+    await fetch("https://eo92jfgk4r4masz.m.pipedream.net", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        cardName: card.name,
+        cardId: card.id,
+        boardName: board.name,
+        driveLink,
+        videoName,
+        flow,
+        triggeredBy: "trello_powerup"
+      })
+    });
+
+    iframe.closePopup();
   });
+};
 
-  t.closePopup();
-});

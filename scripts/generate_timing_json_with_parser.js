9fdbb017251df2abac40c2eebd212ac967ffa2ca
huytvdev <huy8895@gmail.com>
2025-05-17 17:44:51 +0700

    update

--- scripts/generate_timing_json_with_parser.js
+++ scripts/generate_timing_json_with_parser.js
@@ -52,8 +52,29 @@ function normalizeText(text) {
         'seven': '7',
         'eight': '8',
         'nine': '9',
-        'ten': '10'
+        'ten': '10',
+        'eleven': '11',
+        'twelve': '12',
+        'thirteen': '13',
+        'fourteen': '14',
+        'fifteen': '15',
+        'sixteen': '16',
+        'seventeen': '17',
+        'eighteen': '18',
+        'nineteen': '19',
+        'twenty': '20',
+        'twenty-one': '21',
+        'twenty two': '22',
+        'twenty-three': '23',
+        'twenty-four': '24',
+        'twenty-five': '25',
+        'twenty-six': '26',
+        'twenty-seven': '27',
+        'twenty-eight': '28',
+        'twenty-nine': '29',
+        'thirty': '30'
     };
+
 
     // Loại bỏ dấu câu và thay thế từ số bằng số
     const filteredWords = words
